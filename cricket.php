b19533071f47e83863a6f64e1d793ddeca540069
Adharshhari <93755902+Adharshhari@users.noreply.github.com>
2022-01-19 20:08:44 +0530

    Add files via upload

---
+++ cricket.php
@@ -0,0 +1,10 @@
+<?php
+$c = array("Kohli","Rohit Sharma","Sreyas Iyer","Agarwal","Aswin","Bumrah","Chahal","Dhawan","Jadeja","B.Kumar");
+echo "<table border = '1' width = '90'><tr><th>Cricketers</th></tr>";
+foreach($c as $i)
+{
+    echo "<tr><td>$i</td></tr>";
+}
+
+echo "<table>";
+?>
